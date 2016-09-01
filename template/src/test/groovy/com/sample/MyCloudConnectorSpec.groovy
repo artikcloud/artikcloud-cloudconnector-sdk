9a27fd4f96e544d1ebf3af2fd3d652ed5489cda4
David Bernard <d.bernard1@ssi.samsung.com>
2016-09-01 14:56:28 +0200

    OM-3000 template: use space for indentation in MyCloudConnectorSpec

--- template/src/test/groovy/com/sample/MyCloudConnectorSpec.groovy
+++ template/src/test/groovy/com/sample/MyCloudConnectorSpec.groovy
@@ -11,69 +11,69 @@ import utils.FakeContext
 
 class MyCloudConnectorSpec extends Specification {
 
-		def sut = new MyCloudConnector()
-		def ctx = new FakeContext()
+    def sut = new MyCloudConnector()
+    def ctx = new FakeContext()
 
-		def "reject Notification without NotificationId"() {
-			when:
-			def req = new RequestDef("https://foo/cloudconnector/dt00/thirdpartynotification")
-			def res = sut.onNotification(ctx, req)
-			then:
-			res.isBad()
-		}
+    def "reject Notification without NotificationId"() {
+      when:
+      def req = new RequestDef("https://foo/cloudconnector/dt00/thirdpartynotification")
+      def res = sut.onNotification(ctx, req)
+      then:
+      res.isBad()
+    }
 
-		def "accept valid Notification"() {
-			when:
-			def did = 'xxxx'
-			def req = new RequestDef('https://foo/cloudconnector/dt00/thirdpartynotification')
-				.withHeaders(['notificationId': did])
-				.withContent('{"messages":["m1", "m2"]}', 'application/json')
-			def res = sut.onNotification(ctx, req)
+    def "accept valid Notification"() {
+      when:
+      def did = 'xxxx'
+      def req = new RequestDef('https://foo/cloudconnector/dt00/thirdpartynotification')
+        .withHeaders(['notificationId': did])
+        .withContent('{"messages":["m1", "m2"]}', 'application/json')
+      def res = sut.onNotification(ctx, req)
 
-			then:
-			res.isGood()
-			res.get() == new NotificationResponse([
-				new ThirdPartyNotification(new ByDeviceId(did), [
-					new RequestDef("${ctx.parameters()['endpoint']}/messages/m1"),
-					new RequestDef("${ctx.parameters()['endpoint']}/messages/m2")
-				])
-			])
-		}
+      then:
+      res.isGood()
+      res.get() == new NotificationResponse([
+        new ThirdPartyNotification(new ByDeviceId(did), [
+          new RequestDef("${ctx.parameters()['endpoint']}/messages/m1"),
+          new RequestDef("${ctx.parameters()['endpoint']}/messages/m2")
+        ])
+      ])
+    }
 
-		def "send action to cloud when receiving ARTIK Cloud action"() {
-			when:
-			def action = new ActionDef(Option.apply("sdid"), "ddid", System.currentTimeMillis(), "setValue", '{"value":"foo"}')
-			def fakeDevice = new DeviceInfo(
-				"ddid",
-				Option.apply("extId"),
-				new Credentials(AuthType.OAuth2, "", "", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()), ctx.cloudId(), Empty.option()
-			)
-			def actionRes = sut.onAction(ctx, action, fakeDevice)
+    def "send action to cloud when receiving ARTIK Cloud action"() {
+      when:
+      def action = new ActionDef(Option.apply("sdid"), "ddid", System.currentTimeMillis(), "setValue", '{"value":"foo"}')
+      def fakeDevice = new DeviceInfo(
+        "ddid",
+        Option.apply("extId"),
+        new Credentials(AuthType.OAuth2, "", "", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()), ctx.cloudId(), Empty.option()
+      )
+      def actionRes = sut.onAction(ctx, action, fakeDevice)
 
-			then:
-			actionRes.isGood()
-			actionRes.get() == new ActionResponse([
-				new ActionRequest(
-					[
-						new RequestDef("${ctx.parameters().endpoint}/actions/extId/setValue")
-							.withMethod(HttpMethod.Post)
-							.withContent('{"value":"foo"}', "application/json")
-					]
-				)
-			])
-		}
+      then:
+      actionRes.isGood()
+      actionRes.get() == new ActionResponse([
+        new ActionRequest(
+          [
+            new RequestDef("${ctx.parameters().endpoint}/actions/extId/setValue")
+              .withMethod(HttpMethod.Post)
+              .withContent('{"value":"foo"}', "application/json")
+          ]
+        )
+      ])
+    }
 
-		def "reject unkown action"() {
-			when:
-			def action = new ActionDef(Option.apply("sdid"), "ddid", System.currentTimeMillis(), "bar", '{"value":"foo"}')
-			def fakeDevice = new DeviceInfo(
-				"ddid",
-				Option.apply("extId"),
-				new Credentials(AuthType.OAuth2, "", "", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()), ctx.cloudId(), Empty.option()
-			)
-			def actionRes = sut.onAction(ctx, action, fakeDevice)
+    def "reject unkown action"() {
+      when:
+      def action = new ActionDef(Option.apply("sdid"), "ddid", System.currentTimeMillis(), "bar", '{"value":"foo"}')
+      def fakeDevice = new DeviceInfo(
+        "ddid",
+        Option.apply("extId"),
+        new Credentials(AuthType.OAuth2, "", "", Empty.option(), Option.apply("bearer"), ctx.scope(), Empty.option()), ctx.cloudId(), Empty.option()
+      )
+      def actionRes = sut.onAction(ctx, action, fakeDevice)
 
-			then:
-			actionRes.isBad()
-		}
+      then:
+      actionRes.isBad()
+    }
 }
